e507f0b28ddda1c7279d7aedbca93d39afd9fddd
Mathias <mathias.richard.schneider@gmail.com>
2021-07-15 02:34:07 -0500

    integrates home page with theme

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,28 @@
 <template>
-  <div class="home">
+  <div class="intro demo-intro" style="background-image: url(/TGoblinArt.0.jpg)">
+    <div class="gradient-bg"></div>
+    <div class="dtable hw100">
+      <div class="dtable-cell hw100">
+        <div class="container text-center">
+          <h1 class="intro-title animated fadeInDown">Welcome!</h1>
+
+          <p class="sub animateme fittext3 animated fadeIn">
+            Video Game Vault is an all in one site for researching games, adding games to lists, and monitoring
+            inventory for collecting or storefront use.
+          </p>
+
+          <div class="btn-box">
+            <router-link to="/signup" class="btn btn-primary btn-round">Sign Up</router-link>
+            <router-link to="/login" class="btn btn-primary btn-round">Log In</router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- <div class="home">
     <p>HOME PAGE</p>
     <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
+  </div> -->
 </template>
 
 <script>
